5c58e1350f7723e00c311a129a7d90b3645e9d07
Dylan <SuperDyl19@gmail.com>
2022-10-28 16:24:09 -0600

    cleaned up styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { memo } from "react";
+import "./styles.css";
 
 function Footer() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 function NavItem({ link, text, active = false }) {
   const linkClass = "nav-link" + (active ? " active" : "");
